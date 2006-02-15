0b991ff9f0c3b9a9d52627a4853a36469cc85eed
Richard Waid <richard@iopen.net>
2006-02-15 01:09:04 +0000

    redirect to canonicalHost
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%40730

--- Scripts/check/division_level.py
+++ Scripts/check/division_level.py
@@ -12,6 +12,14 @@ RESPONSE =  request.RESPONSE
 if getattr(context, 'supress_redirect', False):
     return True
 
+canonicalHost = context.Scripts.get.option('canonicalHost', None)
+if canonicalHost:
+    base_host = request.BASE0.split('/')[-1]
+    if base_host != canonicalHost:
+        URL = request.URL
+        new_url = URL.replace(base_host, canonicalHost)
+        return RESPONSE.redirect(new_url, lock=1)
+
 user = request.AUTHENTICATED_USER
 if user.getUserName() != 'Anonymous User':
     if getattr(context, 'is_division', False):
@@ -21,4 +29,5 @@ if user.getUserName() != 'Anonymous User':
          nurl = division+request.URLPATH0
          nurl = '/'+'/'.join(filter(None, nurl.split('/')))
          return RESPONSE.redirect(nurl, lock=1)
+
 return True
