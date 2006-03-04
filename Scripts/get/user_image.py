03d3688724a906548e6f848146dae6293daf08e6
Richard Waid <richard@iopen.net>
2006-03-04 09:01:27 +0000

    Removed redundant information
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%40800

--- Scripts/get/user_image.py
+++ Scripts/get/user_image.py
@@ -18,17 +18,3 @@ if user:
     return user.get_image()
 
 return ''
-
-
-user = context.acl_users.getUser(user_id)
-
-imageurl = None
-if user and (not user.getProperty('restrictImage', 1)):
-    for id in ['%s.jpg' % user_id,'%s.JPG' % user_id]:
-        image = getattr(context.contactsimages, id, None)
-        if image:
-            imageurl = image.absolute_url(1)
-            break
-
-return imageurl
-
