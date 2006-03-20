ae351386b50c2d9b834fc09a0a2c8f349049e040
Michael JasonSmith <mpj17@onlinegroups.net>
2006-03-20 03:38:05 +0000

    I have colon trouble.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%40862

--- Scripts/notify/forgottenpassword.py
+++ Scripts/notify/forgottenpassword.py
@@ -23,7 +23,7 @@ if not user and username.find('@') > 0:
 if user:
     # Reset the password and send the user a new one.
     user.reset_password()
-    n_dict = {'server', context.REQUEST.SERVER_URL}
+    n_dict = {'server': context.REQUEST.SERVER_URL}
     user.send_notification('forgotten_password', 'default', n_dict)
     
     return context.REQUEST.RESPONSE.redirect('/sent_password.xml')
