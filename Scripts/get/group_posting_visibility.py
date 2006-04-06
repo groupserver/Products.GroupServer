e176a555d84c8871fb2d98d4ade49194758644cc
Richard Waid <richard@iopen.net>
2006-04-06 05:07:15 +0000

    Posting visibility support
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%40929

---
+++ Scripts/get/group_posting_visibility.py
@@ -0,0 +1,22 @@
+## Script (Python) "group_posting_visibility"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+site_root = context.site_root()
+group_object = context.Scripts.get.group_object()
+
+listManager = site_root.objectValues('XWF Mailing List Manager')[0]
+group = listManager.get_list(group_object.getId())
+
+memberlist = group.lowerList(group.getValueFor('mailinlist'))
+user = context.REQUEST.AUTHENTICATED_USER
+if user.getId():
+    for address in user.get_emailAddresses():
+        if address in memberlist:
+            return 'yes'
+return 'no'
