c095c80bf7d4a19139128e6b65cce6fe997c878a
Michael JasonSmith <mpj17@onlinegroups.net>
2006-05-09 07:56:04 +0000

    Added code to changeIntroduction so it actually changes the introduction. Fingers crossed for commit 1000\ldots
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%401000

--- Scripts/forms/siteinfo/changeIntroduction.py
+++ Scripts/forms/siteinfo/changeIntroduction.py
@@ -8,6 +8,9 @@
 ##title=Change Site Introduction
 ##
 
+# Change the introduction text for the site. This script is not part
+#   of start a site code, they all end in "Next".
+
 result = {}
 
 form = context.REQUEST.form
@@ -23,6 +26,27 @@ for field in form:
 result = container.check_introduction(form['introduction'])
 if result['error']:
     return result
+
+# --=mppj17=--
+# We really should check if division_intro.xml exisits, rather than
+#  assuming it does!
+
+introXML = '''<?xml version="1.0" encoding="utf-8"?>
+<div
+  xmlns:tal="http://xml.zope.org/namespaces/tal"
+  xmlns:metal="http://xml.zope.org/namespaces/metal" 
+  metal:define-macro="division_introduction" 
+  class="introduction">
+
+  <paragraph>
+    %s
+	</paragraph> 
+</div>''' % form['introduction']
+
+divIntro = context.restrictedTraverse('Content/%s/division_introduction.xml' %\
+  siteId)
+
+divIntro.write(introXML)
 
 # No error, redirect
 result['error'] = False
