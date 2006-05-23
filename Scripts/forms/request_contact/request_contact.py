dc5bcaf937b23e9a8c607b182cb935672a93d4e2
Michael JasonSmith <mpj17@onlinegroups.net>
2006-05-23 03:55:13 +0000

    Right code this time.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%401038

--- Scripts/forms/request_contact/request_contact.py
+++ Scripts/forms/request_contact/request_contact.py
@@ -4,21 +4,41 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=user_id=None,site=None
+##parameters=
 ##title=Request Contact from User
 ##
 
+result = {}
+
+form = context.REQUEST.form
+assert form.has_key('contactedUser')
+assert form.has_key('site')
+result['form'] = form
+
+for field in form:
+    try:
+        form[field] = form[field].strip()
+    except AttributeError:
+        pass
+
+
 site_root = context.site_root()
 division_object = context.Scripts.get.division_object() #?
-user = site_root.acl_users.getUser(user_id)
+user = site_root.acl_users.getUser(form['contactedUser'])
 
 email_addresses = user.get_defaultDeliveryEmailAddresses()
 if email_addresses:
-    n_dict = {'requesting_user': context.REQUEST.AUTHENTICATED_USER}
+    n_dict = {'requesting_user': context.REQUEST.AUTHENTICATED_USER,
+              'site': form['site']}
     user.send_notification('request_contact',
                            'default',
                            n_dict=n_dict,
                            email_only=email_addresses)
 
-url = '/%s/contacts/%s?mid=1040' % (division_object.getId(), user_id)
-return context.REQUEST.RESPONSE.redirect(url)
+result['error'] = False
+result['message'] = '''The contact request has been sent.'''
+
+return result
+
+# url = '/%s/contacts/%s?mid=1040' % (division_object.getId(), user_id)
+# return context.REQUEST.RESPONSE.redirect(url)
