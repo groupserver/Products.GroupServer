8b4ea83aa69dfc6f7de6df08e718f17422f93aa2
Michael JasonSmith <mpj17@onlinegroups.net>
2006-06-02 03:20:38 +0000

    Error checking ;)
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%401070

--- Scripts/forms/siteinfo/siteReviewReview.py
+++ Scripts/forms/siteinfo/siteReviewReview.py
@@ -22,6 +22,32 @@ for field in form:
     except AttributeError:
         pass
 
+# Check the three fields. Most of the following is cut 'n' paste from
+#   the "Next" scripts
+
+# - Site name
+#   Remove Online Groups from the end of the name
+if (form['sitename'][-13:].lower() == 'online groups'):
+    form['sitename'] = form['sitename'][:-13].strip()
+#   Check for errors in the site name
+result = container.check_name(form['sitename'])
+if result['error']:
+    return result
+
+# - Subdomain
+#   Remove .onlinegroups.net from the end of the name; in fact,
+#   remove everything after the first ".".
+subdomain = form['subdomain'].split('.')[0].lower()
+result = container.check_subdomain(subdomain)
+if result['error']:
+    return result
+
+# - Introduction
+result = container.check_introduction(form['introduction'])
+if result['error']:
+    return result
+
+
 context.REQUEST.RESPONSE.redirect('sitereview.xml?sitename=%s&subdomain=%s&introduction=%s' %
                                   (form['sitename'], form['subdomain'],
                                    form['introduction']))
