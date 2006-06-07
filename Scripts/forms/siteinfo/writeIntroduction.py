c61719606732b23fd72bdb47704b6ea23965a346
Michael JasonSmith <mpj17@onlinegroups.net>
2006-06-07 07:21:19 +0000

    Refactoring.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%401076

---
+++ Scripts/forms/siteinfo/writeIntroduction.py
@@ -0,0 +1,34 @@
+## Script (Python) "writeIntroduction"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=introduction=''siteId=''
+##title=Change Site Introduction
+##
+
+# We do not care if the introduction is empty
+assert siteId != ''
+
+introXML = '''<?xml version="1.0" encoding="utf-8"?>
+<div
+  xmlns:tal="http://xml.zope.org/namespaces/tal"
+  xmlns:metal="http://xml.zope.org/namespaces/metal" 
+  metal:define-macro="division_introduction" 
+  class="introduction">
+
+  <paragraph>
+    %s
+	</paragraph> 
+</div>''' % introduction
+
+introFile = 'Content/%s/division_introduction.xml' % siteId
+divIntro = context.restrictedTraverse(introFile)
+
+divIntro.write(introXML)
+
+# No error
+result['error'] = False
+result['message'] = 'The site introduction has been changed.'
+return result
