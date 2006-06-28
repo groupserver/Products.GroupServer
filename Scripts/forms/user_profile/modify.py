cbab8d9cc3ecc0a915409ce46595ac1f2797220b
Michael JasonSmith <mpj17@onlinegroups.net>
2006-06-28 04:05:01 +0000

    Damn python error messages are causing me problems :)
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%401115

--- Scripts/forms/user_profile/modify.py
+++ Scripts/forms/user_profile/modify.py
@@ -8,6 +8,18 @@
 ##title=
 ##
 from Products.XWFCore import validators as v
+
+# The following is nicked, verbatim, from cgi.py, which ships with
+# Python2.4 
+def escape(s, quote=None):
+    """Replace special characters '&', '<' and '>' by SGML entities."""
+    s = s.replace("&", "&amp;") # Must be done first!
+    s = s.replace("<", "&lt;")
+    s = s.replace(">", "&gt;")
+    if quote:
+        s = s.replace('"', "&quot;")
+    return s
+
 required = {}
             
 validators = {}
@@ -45,10 +57,12 @@ for key in form.keys():
     if key[0] == '_':
         continue
     prop = getattr(context.UserProperties.aq_explicit, key, None)
+    text = form[key]
     if prop and user.hasProperty(prop.getId()):
-        user.manage_changeProperties({key:form[key]})
+        user.manage_changeProperties({key:text})
     elif prop:
-        user.manage_addProperty(prop.getId(), form[key], prop.getProperty('property_type'))
+        user.manage_addProperty(prop.getId(), text,
+                                prop.getProperty('property_type'))
 
 result['message'] = "<p>Successfully modified profile</p>"
 
