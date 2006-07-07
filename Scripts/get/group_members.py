0b75801a33bc8edf882d6d0e89c7fcd6aab2887b
Michael JasonSmith <mpj17@onlinegroups.net>
2006-07-07 02:35:55 +0000

    Now group_members.py filters out None users before returing the users as a list.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%401122

--- Scripts/get/group_members.py
+++ Scripts/get/group_members.py
@@ -9,11 +9,14 @@
 ##
 
 def sorter(a,b):
-    if a.getProperty('lastName') > b.getProperty('lastName'):
-        return 1
-    else:
-        return -1
-
+   if a ==None or b == None:
+      return 0
+   
+   if a.getProperty('lastName') > b.getProperty('lastName'):
+      return 1
+   else:
+      return -1
+   
 users = []
 site_object = context.site_root()
 group_object = context.Scripts.get.group_object()
@@ -25,4 +28,6 @@ for group_id in group_object.groups_with_local_role('GroupMember'):
         users.append(site_object.acl_users.getUser(user_id))
 
 users.sort(sorter)
+users = filter(lambda u: u != None, users)
+assert None not in users
 return users
