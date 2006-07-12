2c984353dd96309fe4e7a69ebd47394e50d41d5f
Michael JasonSmith <mpj17@onlinegroups.net>
2006-07-12 00:00:04 +0000

    Now can add and remove moderators from the group.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%401130

--- Scripts/forms/manage_group_members/moderate/remove_users.py
+++ Scripts/forms/manage_group_members/moderate/remove_users.py
@@ -18,31 +18,38 @@ site_root = context.site_root()
 group = context.Scripts.get.group_by_id(groupid)
 assert(group != None)
 
-
 listManager = site_root.objectValues('XWF Mailing List Manager')[0]
 grouplist = getattr(listManager, groupid)
 assert(grouplist != None)
 
-mmembers = []
-if grouplist.hasProperty('moderated_members'):
-   mmembers = filter(None, grouplist.getProperty('moderated_members'))
+mmembers = group.Scripts.get.group_moderators(ids_only=True)
 
 for member in userids:
-    if member in mmembers:
-        mmembers.remove(member)
+   if member in mmembers:
+      mmembers.remove(member)
 
-if grouplist.hasProperty('moderated_members'):
-    grouplist.manage_changeProperties(moderated_members=mmembers)
+moderatorEmailAddresses = []
+for memberId in mmembers:
+   member = site_root.acl_users.getUser(memberId)
+   email = member.get_defaultDeliveryEmailAddresses()[0]
+   moderatorEmailAddresses.append(email)
+      
+if grouplist.hasProperty('moderator'):
+    grouplist.manage_changeProperties(moderator=moderatorEmailAddresses)
 else:
-    grouplist.manage_addProperty('moderated_members', mmembers, 'lines')
+    grouplist.manage_addProperty('moderator', moderatorEmailAddresses,
+                                 'lines')
 
 userNames = ', '.join(map(lambda m: group.Scripts.get.user_realnames(m),
                           userids))
 result['error'] = False
 m = (len(userids) == 1) and ('member %s has' % userNames)\
     or ('%d members (%s) have' % (len(userids), userNames))
-result['message'] = '''<paragraph>The %s been <em>removed</em>
-  from the moderation list for %s.
-  There are now %d moderated members of
-  this group.</paragraph>''' % (m, group.title_or_id(), len(mmembers))
+isOrAre = ((len(mmembers) == 1) and 'is') or 'are'
+sOrNul =  ((len(mmembers) != 1) and 's') or ''
+result['message'] = '''<paragraph>The %s had the moderation status
+   for %s <em>revoked</em>.
+  There %s now %d moderator%s for
+  this group.</paragraph>''' % (m, group.title_or_id(), isOrAre,
+                                len(mmembers), sOrNul)
 return result
