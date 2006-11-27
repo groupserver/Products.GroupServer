4e1891b7f5c5be806f6896848d06215a1e056ec2
Michael JasonSmith <mpj17@onlinegroups.net>
2006-11-27 02:48:00 +0000

    Clean up so the public groups behave correctly!
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%401430

--- Scripts/get/group_messages_visibility.py
+++ Scripts/get/group_messages_visibility.py
@@ -13,7 +13,18 @@ messages_object = getattr(group_object, 'messages', None)
 if not messages_object:
     return 'nobody'
 
-view_roles = filter(None, map(lambda x: x['selected'] and x['name'] or None, messages_object.rolesOfPermission('View')))
+view_roles = filter(None, map(lambda x: x['selected'] and x['name'] or None,
+                              messages_object.rolesOfPermission('View')))
+
+if 'Anonymous' in view_roles and 'Authenticated' in view_roles:
+    return 'anyone'
+elif 'DivisionMember' in view_roles:
+    return 'division'
+elif 'GroupMember' in view_roles:
+    return 'group'
+
+view_roles = filter(None, map(lambda x: x['selected'] and x['name'] or None,
+                              group_object.rolesOfPermission('View')))
 
 if 'Anonymous' in view_roles and 'Authenticated' in view_roles:
     return 'anyone'
