b4c4b3ab57ddc8efd1364a1a8c6c54f40117cada
Richard Waid <richard@iopen.net>
2007-07-16 10:55:43 +0000

    Some tidyup
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%402298

--- Scripts/get/group_memberships_ids_all.py
+++ Scripts/get/group_memberships_ids_all.py
@@ -14,25 +14,23 @@ for division_object in division_objects:
     if not groups:
         continue
     memberships = context.group_memberships(division_object.groups, user_id)
-    #if gtype:
-    #    memberships = memberships.get(gtype, [])
-    #else:
-    if 1:
-        m = []
-        for gtype in memberships:
-            m += memberships.get(gtype, [])
-        memberships = m
+   
+    m = []
+    for gtype in memberships:
+        m += memberships.get(gtype, [])
+    memberships = m
     
+    canonicalHost = division_object.Scripts.get.option('canonicalHost')
+    divisionTitle = division_object.title_or_id()
     for membership in memberships:
-        if not membership.getId() in group_ids:
-            groupHost = division_object.Scripts.get.option('canonicalHost')
-            #groupUrl = "http://%s/groups/%s" % (groupHost, membership.getId())
+        membership_id = membership.getId()
+        if not membership_id in group_ids:
             groupUrl = "/%s" % membership.absolute_url(1)
             group_title_ids.append((membership.getProperty('title'),
-                                    membership.getId(),
+                                    membership_id,
                                     groupUrl,
-                                    division_object.title_or_id(),
-                                    groupHost)) 
-            group_ids.append(membership.getId())
+                                    divisionTitle,
+                                    canonicalHost)) 
+            group_ids.append(membership_id)
     
 return group_title_ids
