faf093b5b81e341afb00e8e3432117fc9653981b
Alice Murphy <alice@onlinegroups.net>
2007-07-23 22:59:00 +0000

    modified to accommodate those sites without a support group
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%402326

--- Scripts/get/support_email.py
+++ Scripts/get/support_email.py
@@ -8,7 +8,8 @@
 ##title=Get Support Email
 ##
 
-user = context.REQUEST.AUTHENTICATED_USER
+site_root = context.site_root()
+site = getattr(site_root.Content, siteId, None)
 division_config = getattr(context, 'DivisionConfiguration', None)
 assert division_config, 'Division Config set to %s' % division_config
 global_config = getattr(context, 'GlobalConfiguration', None)
@@ -20,10 +21,16 @@ canonical = division_config.getProperty('canonicalHost', 'onlinegroups.net')
 if global_support_email != 'support@onlinegroups.net':    # if we're on an instance other than onlinegroups.net
     retval = global_config.supportEmail
 else:                                                     # if we're on the onlinegroups.net instance
-    if canonical == '%s.onlinegroups.net' % siteId:
-        retval = '%s_support@onlinegroups.net' % siteId     # if the site uses a subdomain of onlinegroups.net
-    elif canonical != 'onlinegroups.net':
-        retval = '%s_support@%s' % (siteId, canonical)      # if the site is has a custom domain
+    if canonical == '%s.onlinegroups.net' % siteId:       # if the site uses a subdomain of onlinegroups.net
+        if hasattr(site.groups, '%s_support' % siteId):
+            retval = '%s_support@onlinegroups.net' % siteId   # and if the site has a support group
+        else:
+            retval = 'support@onlinegroups.net'               # otherwise support@onlinegroups.net
+    elif canonical != 'onlinegroups.net':                 # if the site is has a custom domain
+        if hasattr(site.groups, '%s_support' % siteId):
+            retval = '%s_support@%s' % (siteId, canonical)    # and if the site has a support group
+        else:
+            retval = 'support@onlinegroups.net'               # otherwise support@onlinegroups.net
     else:
         retval = 'support@onlinegroups.net'
 
