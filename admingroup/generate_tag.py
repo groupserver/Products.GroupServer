e25491cbe5a72a1104eb740402550d084fb70ee8
Alice Murphy <alice@onlinegroups.net>
2007-10-02 03:24:16 +0000

    Acquisition sure can bite. Tidy-up of code also.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%402626

--- admingroup/generate_tag.py
+++ admingroup/generate_tag.py
@@ -9,23 +9,43 @@
 ##
 from Products.PythonScripts.standard import html_quote
 
+assert tag_type, \
+    "Did not receive tag_type"
+assert tag_fields, \
+    "Did not receive tag_fields dictionary for tag_type %s" % tag_type
+
 if tag_fields.has_key('tag_value'):
-    try:
-        tag_fields['tag_value'].lower
+    if hasattr(tag_fields['tag_value'], 'lower'):
         tag_fields['tag_value'] = html_quote(tag_fields['tag_value'])
-    except:
-        tf = []
-        for val in tag_fields['tag_value']:
-            tf.append(html_quote(val))
-        tag_fields['tag_value'] = tf        
+    else:
+        try:
+            tf = []
+            for val in tag_fields['tag_value']:
+                tf.append(html_quote(val))
+            tag_fields['tag_value'] = tf
+        except:
+            assert False, \
+              'Error creating tag for property %s' % tag_fields['tag_id']
 
 if tag_type == 'model_string':
+    assert tag_fields.has_key('tag_id'), \
+        "No key 'tag_id' in dictionary tag_fields for tag_type 'model_string'"
+    assert tag_fields.has_key('tag_value'), \
+        "No key 'tag_value' in dictionary 'tag_fields': tag_id %s" % tag_fields['tag_id']
     return """<%(tag_id)s>%(tag_value)s</%(tag_id)s>""" % tag_fields
 
 elif tag_type == 'model_text':
+    assert tag_fields.has_key('tag_id'), \
+        "No key 'tag_id' in dictionary tag_fields for tag_type 'model_text'"
+    assert tag_fields.has_key('tag_value'), \
+        "No key 'tag_value' in dictionary 'tag_fields': tag_id %s" % tag_fields['tag_id']
     return """<%(tag_id)s>%(tag_value)s</%(tag_id)s>""" % tag_fields
 
 elif tag_type == 'model_lines':
+    assert tag_fields.has_key('tag_id'), \
+        "No key 'tag_id' in dictionary tag_fields for tag_type 'model_lines'"
+    assert tag_fields.has_key('tag_value'), \
+        "No key 'tag_value' in dictionary 'tag_fields': tag_id %s" % tag_fields['tag_id']
     tag_fields['tag_value'] = '\n'.join(tag_fields['tag_value'])
     return """<%(tag_id)s>%(tag_value)s</%(tag_id)s>""" % tag_fields
 
