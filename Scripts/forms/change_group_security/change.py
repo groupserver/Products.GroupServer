53f113bd71a75cb643d746e27e7c4d456633a1f1
Alice Murphy <alice@onlinegroups.net>
2007-10-08 22:35:41 +0000

    Messages and files visibility now linked.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%402651

--- Scripts/forms/change_group_security/change.py
+++ Scripts/forms/change_group_security/change.py
@@ -13,9 +13,8 @@ result = {}
 form = context.REQUEST.form
 assert form.has_key('groupid')
 assert form.has_key('siteid')
-assert form.has_key('permvisibility')
 assert form.has_key('permjoin')
-assert form.has_key('permviewfiles')
+assert form.has_key('permvisibility')
 assert form.has_key('permviewmessages')
 result['form'] = form
 
@@ -41,22 +40,6 @@ if group.Scripts.get.division_object().getId() != siteid:
 listManager = site_root.objectValues('XWF Mailing List Manager')[0]
 grouplist = getattr(listManager, groupid)
 
-visibility = group.Scripts.get.group_visibility()
-permvisibility = form.get('permvisibility', 'group')
-if visibility != permvisibility:
-    if permvisibility == 'anyone':
-        group.manage_permission('View', ['Anonymous', 'Authenticated', 'DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        group.manage_permission('Access contents information', ['Anonymous', 'Authenticated', 'DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        message.append('<li>Updated group permissions to make the group visible to anyone</li>')
-    elif permvisibility == 'division':
-        group.manage_permission('View', ['DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        group.manage_permission('Access contents information', ['DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        message.append('<li>Updated group permissions to make the group visible to only division members</li>')    
-    elif permvisibility == 'group':
-        group.manage_permission('View', ['DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        group.manage_permission('Access contents information', ['DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        message.append('<li>Updated group permissions to make the group visible to only group members</li>')
-
 joinability = group.Scripts.get.group_joinability()
 permjoinability = form.get('permjoin', 'group')
 if joinability != permjoinability:
@@ -72,45 +55,43 @@ if joinability != permjoinability:
     
     message.append('<li>Updated group joinability</li>')
 
-filesvisibility = group.Scripts.get.group_files_visibility()
-permviewfiles = form.get('permviewfiles', 'group')
-if filesvisibility != permviewfiles:
-    if permviewfiles == 'default':
-        group.files.manage_permission('View', [], 1)
-        group.files.manage_permission('Access contents information', [], 1)
-        message.append('<li>Updated file area permissions to restore to group defaults</li>')
-    elif permviewfiles == 'anyone':
-        group.files.manage_permission('View', ['Anonymous', 'Authenticated', 'DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        group.files.manage_permission('Access contents information', ['Anonymous', 'Authenticated', 'DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        message.append('<li>Updated file area permissions to make the files visible to anyone</li>')
-    elif permviewfiles == 'division':
-        group.files.manage_permission('View', ['DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        group.files.manage_permission('Access contents information', ['DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        message.append('<li>Updated file area permissions to make the files visible to only division and group members</li>')    
-    elif permviewfiles == 'group':
-        group.files.manage_permission('View', ['DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        group.files.manage_permission('Access contents information', ['DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        message.append('<li>Updated file area permissions to make the files visible to only group members</li>')
+visibility = group.Scripts.get.group_visibility()
+permvisibility = form.get('permvisibility', 'group')
+if visibility != permvisibility:
+    if permvisibility == 'anyone':
+        group.manage_permission('View', ['Anonymous', 'Authenticated', 'DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        group.manage_permission('Access contents information', ['Anonymous', 'Authenticated', 'DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        message.append('<li>Updated permissions to make the group visible to anyone</li>')
+    elif permvisibility == 'division':
+        group.manage_permission('View', ['DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        group.manage_permission('Access contents information', ['DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        message.append('<li>Updated permissions to make the group visible to site members</li>')    
+    elif permvisibility == 'group':
+        group.manage_permission('View', ['DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        group.manage_permission('Access contents information', ['DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        message.append('<li>Updated permissions to make the group visible to group members only</li>')
 
 messagevisibility = group.Scripts.get.group_messages_visibility()
 permviewmessages = form.get('permviewmessages', 'group')
 if messagevisibility != permviewmessages:
-    if permviewmessages == 'default':
-        group.messages.manage_permission('View', [], 1)
-        group.messages.manage_permission('Access contents information', [], 1)
-        message.append('<li>Updated messages area permissions to restore to group defaults</li>')
-    elif permviewmessages == 'anyone':
+    if permviewmessages == 'anyone':
         group.messages.manage_permission('View', ['Anonymous', 'Authenticated', 'DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
         group.messages.manage_permission('Access contents information', ['Anonymous', 'Authenticated', 'DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        message.append('<li>Updated messages area permissions to make the messages visible to anyone</li>')
+        group.files.manage_permission('View', ['Anonymous', 'Authenticated', 'DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        group.files.manage_permission('Access contents information', ['Anonymous', 'Authenticated', 'DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        message.append('<li>Updated permissions to make the messages and files visible to anyone</li>')
     elif permviewmessages == 'division':
         group.messages.manage_permission('View', ['DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
         group.messages.manage_permission('Access contents information', ['DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        message.append('<li>Updated messages area permissions to make the messages visible to only division and group members</li>')    
+        group.files.manage_permission('View', ['DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        group.files.manage_permission('Access contents information', ['DivisionMember', 'DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        message.append('<li>Updated permissions to make the messages and files visible to site and group members only</li>')    
     elif permviewmessages == 'group':
         group.messages.manage_permission('View', ['DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
         group.messages.manage_permission('Access contents information', ['DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
-        message.append('<li>Updated messages area permissions to make the messages visible to only group members</li>')
+        group.files.manage_permission('View', ['DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        group.files.manage_permission('Access contents information', ['DivisionAdmin','GroupAdmin','GroupMember','Manager', 'Owner'])
+        message.append('<li>Updated permissions to make the messages and files visible to group members only</li>')
 
 m = '''<p>Updating group %s</p>
   <ul>%s</ul>''' % (html_quote(group.title_or_id()), '\n'.join(message))
