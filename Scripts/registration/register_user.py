c132392322bc7f653c06842ba0ad9aded25e2b5a
Alice Murphy <alice@onlinegroups.net>
2007-12-13 02:26:53 +0000

    Context problems.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%402922

--- Scripts/registration/register_user.py
+++ Scripts/registration/register_user.py
@@ -10,10 +10,21 @@
 from Products.XWFCore.XWFUtils import createRequestFromRequest, getOption
 from Products.XWFCore.XWFUtils import get_support_email, get_site_by_id
 
+assert preferred_name, 'No preferred name'
+assert email, 'No email'
+assert user_id, 'No user id'
+assert site_id, 'No site id'
+assert groups, 'No groups'
+
+
 redirect = context.REQUEST.RESPONSE.redirect
+assert redirect, 'No redirect'
 form = context.REQUEST.form
+assert form, 'No form'
 
 site_root = context.site_root()
+assert site_root, 'No site root'
+
 error = []
 if not preferred_name:
     error.append('error:list=pname')
@@ -21,7 +32,7 @@ if not email:
     error.append('error:list=email')
 
 if manual:
-    for prop_def in context.UserProperties.objectValues():
+    for prop_def in site_root.UserProperties.objectValues():
         if prop_def and prop_def.getId() not in ['email', 'preferred_name', 'user_id']:
             if prop_def.getProperty('required', 0) and not form.get(prop_def.getId(), None):
                 error.append('error:list=required')
@@ -34,7 +45,7 @@ print group_string
 if error:
     if manual:
         error_string = '&'.join(error)
-        rstring = str('/login/register?%s&%s&%s' % (error_string, createRequestFromRequest(context.REQUEST, preferred_name=preferred_name, email=email, user_id=user_id), group_string))
+        rstring = str('/login/register.xml?%s&%s&%s' % (error_string, createRequestFromRequest(context.REQUEST, preferred_name=preferred_name, email=email, user_id=user_id), group_string))
         print 'Will redirect to %s' % rstring
         return redirect(rstring)
     return 0
@@ -58,8 +69,8 @@ except Exception, x:
 if error:
     if manual:
         error_string = '&'.join(error)
-        print 'Will redirect to /login/register?%s&came_from=%s&preferred_name=%s&email=%s&user_id=%s&%s' % (error_string, came_from, preferred_name, email, user_id, group_string)
-        return redirect('/login/register?%s&came_from=%s&preferred_name=%s&email=%s&user_id=%s&%s' % (error_string, came_from, preferred_name, email, user_id, group_string))
+        print 'Will redirect to /login/register.xml?%s&came_from=%s&preferred_name=%s&email=%s&user_id=%s&%s' % (error_string, came_from, preferred_name, email, user_id, group_string)
+        return redirect('/login/register.xml?%s&came_from=%s&preferred_name=%s&email=%s&user_id=%s&%s' % (error_string, came_from, preferred_name, email, user_id, group_string))
     return 0
     
 user = site_root.acl_users.getUser(user_id)
@@ -67,7 +78,7 @@ print 'User is %s' % user.getId()
 
 for prop in userproperties.keys():
     print 'Adding user properties from userproperties dictionary'
-    prop_def = getattr(context.UserProperties.aq_explicit, prop, None)
+    prop_def = getattr(site_root.UserProperties.aq_explicit, prop, None)
     if prop_def and prop != 'email':
         if user.hasProperty(prop):
             user.manage_changeProperties({prop: userproperties[prop]})
@@ -77,7 +88,7 @@ for prop in userproperties.keys():
 for prop in form:
     print 'Adding user properties from form'
     try:
-        prop_def = getattr(context.UserProperties.aq_explicit, prop, None)
+        prop_def = getattr(site_root.UserProperties.aq_explicit, prop, None)
     except:
         continue
     
@@ -112,7 +123,7 @@ else:
         'password'      : password,
         'canonical'     : canonical,
         'siteName'      : site.title_or_id(),
-        'supportEmail'  : get_support_email(context, site_id)
+        'supportEmail'  : get_support_email(site, site_id)
     }
     print 'Sending admin_verified_join notification'
     user.send_notification(n_type='admin_verified_join', n_id='default', n_dict=n_dict)
@@ -125,5 +136,5 @@ if manual:
         print 'Redirecting to /login/index.xml?error:list=register_thanks'
         return redirect('/login/index.xml?error:list=register_thanks')
 
-#return user
-return printed
+return user
+#return printed
