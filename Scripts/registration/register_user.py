6496e75883254e1d828fde8bca120022e983b3a7
Alice Murphy <alice@onlinegroups.net>
2007-12-13 22:04:17 +0000

    Removing debugging statements.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%402926

--- Scripts/registration/register_user.py
+++ Scripts/registration/register_user.py
@@ -10,20 +10,11 @@
 from Products.XWFCore.XWFUtils import createRequestFromRequest, getOption
 from Products.XWFCore.XWFUtils import get_support_email, get_site_by_id
 
-assert preferred_name, 'No preferred name'
-assert email, 'No email'
-assert user_id, 'No user id'
-assert site_id, 'No site id'
-assert groups, 'No groups'
-
 
 redirect = context.REQUEST.RESPONSE.redirect
-assert redirect, 'No redirect'
 form = context.REQUEST.form
-assert form, 'No form'
 
 site_root = context.site_root()
-assert site_root, 'No site root'
 
 error = []
 if not preferred_name:
@@ -36,17 +27,13 @@ if manual:
         if prop_def and prop_def.getId() not in ['email', 'preferred_name', 'user_id']:
             if prop_def.getProperty('required', 0) and not form.get(prop_def.getId(), None):
                 error.append('error:list=required')
-                print 'Missing a required field'
 
-print groups
 group_string = '&'.join(map(lambda x: 'groups:list=%s' % x.split('_member')[0], groups))
-assert '_member' not in group_string, 'group_string contains errors: %s' % group_string
 
 if error:
     if manual:
         error_string = '&'.join(error)
         rstring = str('/login/register.xml?%s&%s&%s' % (error_string, createRequestFromRequest(context.REQUEST, preferred_name=preferred_name, email=email, user_id=user_id), group_string))
-        print 'Will redirect to %s' % rstring
         return redirect(rstring)
     return 0
 
@@ -69,15 +56,13 @@ except Exception, x:
 if error:
     if manual:
         error_string = '&'.join(error)
-        print 'Will redirect to /login/register.xml?%s&came_from=%s&preferred_name=%s&email=%s&user_id=%s&%s' % (error_string, came_from, preferred_name, email, user_id, group_string)
-        return redirect('/login/register.xml?%s&came_from=%s&preferred_name=%s&email=%s&user_id=%s&%s' % (error_string, came_from, preferred_name, email, user_id, group_string))
+        rstring = str('/login/register.xml?%s&came_from=%s&preferred_name=%s&email=%s&user_id=%s&%s' % (error_string, came_from, preferred_name, email, user_id, group_string))
+        return redirect(rstring)
     return 0
     
 user = site_root.acl_users.getUser(user_id)
-print 'User is %s' % user.getId()
 
 for prop in userproperties.keys():
-    print 'Adding user properties from userproperties dictionary'
     prop_def = getattr(site_root.UserProperties.aq_explicit, prop, None)
     if prop_def and prop != 'email':
         if user.hasProperty(prop):
@@ -86,7 +71,6 @@ for prop in userproperties.keys():
             user.manage_addProperty(prop, userproperties[prop], prop_def.getProperty('property_type'))
 
 for prop in form:
-    print 'Adding user properties from form'
     try:
         prop_def = getattr(site_root.UserProperties.aq_explicit, prop, None)
     except:
@@ -99,11 +83,9 @@ for prop in form:
             user.manage_addProperty(prop, form[prop], prop_def.getProperty('property_type'))
 
 if groups:
-    print 'Setting verification groups: %s' % groups
     user.set_verificationGroups(groups)
 
 if sendVerification:
-    print 'Sending verification'
     user.send_userVerification(password=password, site=context.REQUEST.SERVER_URL)
 else:
     # --=mpj17=-- I should be punished for this:
@@ -125,16 +107,15 @@ else:
         'siteName'      : site.title_or_id(),
         'supportEmail'  : get_support_email(site, site_id)
     }
-    print 'Sending admin_verified_join notification'
     user.send_notification(n_type='admin_verified_join', n_id='default', n_dict=n_dict)
 
 if manual:
     if came_from:
-        print 'Redirecting to /login/index.xml?error:list=register_thanks&came_from=%s' % came_from
-        return redirect('/login/index.xml?error:list=register_thanks&came_from=%s' % came_from)
+        rstring = str('/login/index.xml?error:list=register_thanks&came_from=%s' % came_from)
+        return redirect(rstring)
     else:
-        print 'Redirecting to /login/index.xml?error:list=register_thanks'
-        return redirect('/login/index.xml?error:list=register_thanks')
+        rstring = '/login/index.xml?error:list=register_thanks'
+        return redirect(rstring)
 
 return user
-#return printed
+
