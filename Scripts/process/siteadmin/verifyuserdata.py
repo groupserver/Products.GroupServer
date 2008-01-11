4b95d56734bb86e556b1e8b91fd39bbe4dc2e6de
Alice Murphy <alice@onlinegroups.net>
2008-01-11 01:56:52 +0000

    Oops.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%403033

--- Scripts/process/siteadmin/verifyuserdata.py
+++ Scripts/process/siteadmin/verifyuserdata.py
@@ -27,12 +27,16 @@ else:
         error = 1
         message.append("""<li>A user is already registered with that email address</li>""")
     
-if getattr(site_root.UserProperties.givenName, 'required', 0) and not given_name:
-    error = 1
-    message.append("""<li>The user's first name was not specified</li>""")
+if hasattr(site_root.UserProperties.givenName, 'required'):
+    isRequired = getattr(site_root.UserProperties.givenName, 'required').getProperty('required', 0)
+    if isRequired and not given_name:
+        error = 1
+        message.append("""<li>The user's first name was not specified</li>""")
 
-if getattr(site_root.UserProperties.familyName, 'required', 0) and not family_name:
-    error = 1
-    message.append("""<li>The user's last name was not specified</li>""")
+if hasattr(site_root.UserProperties.familyName, 'required'):
+    isRequired = getattr(site_root.UserProperties.familyName, 'required').getProperty('required', 0)
+    if isRequired and not family_name:
+        error = 1
+        message.append("""<li>The user's last name was not specified</li>""")
     
 return message
