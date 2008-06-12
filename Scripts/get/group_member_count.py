c3f709048aa0d7c8a3b4758aa1d794ca57c43692
Alice Murphy <alice@onlinegroups.net>
2008-06-12 05:27:02 +0000

    Changing the method to get the number of group members.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%403838

--- Scripts/get/group_member_count.py
+++ Scripts/get/group_member_count.py
@@ -12,15 +12,14 @@ site_object = context.site_root()
 group_object = group_object or context.Scripts.get.group_object()
 
 count = 0
-for group_id in group_object.groups_with_local_role('GroupMember'):
-    # Count the number of members in the online group, but do not
-    #   worry if there is no user-group corresponding to the online
-    #   group.
-    try:
-        group = site_object.acl_users.getGroupById(group_id)
-        user_ids = group.getUsers()
-        count += len(user_ids)
-    except:
-        pass
+# Count the number of members in the online group, but do not
+#   worry if there is no user-group corresponding to the online
+#   group.
+try:
+    group = site_object.acl_users.getGroupById('%s_member' % group_object.getId())
+    user_ids = group.getUsers()
+    count += len(user_ids)
+except:
+    pass
 
 return count
