82497b9c032182f109d63795a27886095488a41f
Richard Waid <richard@iopen.net>
2008-10-01 04:54:13 +0000

    use GSGroupsInfo adapter
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GroupServerTemplates/trunk%404262

--- Scripts/get/groups.py
+++ Scripts/get/groups.py
@@ -7,17 +7,24 @@
 ##parameters=groups_object=None
 ##title=
 ##
-groups_object = groups_object or context.Scripts.get.groups_object()
-groups = []
-if not groups_object:
-    return groups
+from Products.GSContent.groupsInfo import GSGroupsInfo
 
-for id in groups_object.objectIds(('Folder', 'Folder (Ordered)')):
-    try:
-        object = getattr(groups_object, id)
-        groups.append((object.title_or_id().lower(), object))
-    except:
-        pass
+visible_groups = GSGroupsInfo(context).get_visible_groups() or []
+
+#groups_object = groups_object or context.Scripts.get.groups_object()
+#groups = []
+#if not groups_object:
+#    return groups
+
+#for id in groups_object.objectIds(('Folder', 'Folder (Ordered)')):
+#    try:
+#        object = getattr(groups_object, id)
+#        groups.append((object.title_or_id().lower(), object))
+#    except:
+#        pass
+
+for group in visible_groups:
+    groups.append((group.title_or_id().lower(), group))
 
 # quickly sort alphabetically -- we do this to avoid
 # fetching the title_or_id multiple times
