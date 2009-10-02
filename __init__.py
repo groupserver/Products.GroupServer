bf1c56e27fe178c38a1f4268560934abfe5e310c
Richard Waid <richard@iopen.net>
2009-10-03 00:27:21 +1300

    somehow accidentally deleted __init__.py
    --HG--
    branch : GroupServerTemplates-vida

---
+++ __init__.py
@@ -0,0 +1,27 @@
+import os
+from Products.FileSystemSite.DirectoryView import registerDirectory
+from AccessControl import ModuleSecurityInfo
+
+from pathutil import get_groupserver_path
+
+import groupserver
+
+# registerDirectory is also in here to get around some serious chicken-egg
+# problems with getting the module path
+def initialize( context ):
+    registerDirectory( 'Scripts', globals() )
+    registerDirectory( 'Presentation', globals() )
+    registerDirectory( 'help', globals() )
+    registerDirectory( 'admindivision', globals() )
+    registerDirectory( 'admingroup', globals() )
+
+    context.registerClass( 
+        groupserver.GroupserverSite, 
+        constructors = ( groupserver.manage_addGroupserverSiteForm, 
+                         groupserver.manage_addGroupserverSite ),
+        icon='icons/ic-groupserversite.png'
+                           )
+                           
+pathutil_security = ModuleSecurityInfo('Products.GroupServer.pathutil')
+pathutil_security.declarePublic('get_groupserver_path')
+
