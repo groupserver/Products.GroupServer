ce97abbc2ec1dfa268a60d2d85bae433cb405ed5
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-15 14:40:17 -0700

    Updating the egg metadata

--- setup.py
+++ setup.py
@@ -12,17 +12,22 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='Products.GroupServer',
     version=version,
     description="Base GroupServer product",
-    long_description=open("README.txt").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
+    long_description=long_description,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -37,7 +42,9 @@ setup(name='Products.GroupServer',
     keywords='groupserver',
     author='Richard Waid',
     author_email='richard@iopen.net',
-    url='http://groupserver.org',
+    maintainer='Michael JasonSmith',
+    maintainer_email='mpj17@onlinegroups.net',
+      url='https://source.iopen.net/groupserver/Products.GroupServer/',
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['Products'],
